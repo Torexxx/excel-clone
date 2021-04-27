5f7e279b68bb57ab04c95cbc76a83529435195d2
torexxx <molento-studio@yandex.ru>
2021-04-27 23:21:32 +0300

    basic config, webpack plugins

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].bundle.js"
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src', 'core'),
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new FaviconsWebpackPlugin('./favicon.ico')
+    ],
+};

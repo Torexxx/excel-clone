aa8afa9e2b36d6d1f85832aa51dee85d12ef43e8
torexxx <molento-studio@yandex.ru>
2021-04-30 01:50:56 +0300

    build modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,65 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+module.exports = (env, argv) => {
 
-module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        main: './index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "[name].bundle.js"
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src', 'core'),
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new FaviconsWebpackPlugin('./favicon.ico'),
-        new MiniCssExtractPlugin({
-            filename: '[name].bundle.css',
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+    const isProd = argv.mode === 'production';
+    const isDev = !isProd;
+
+    const filename = ext => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
+
+    return {
+        context: path.resolve(__dirname, 'src'),
+        entry: {
+            main: './index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: filename('js'),
+            clean: true
+        },
+
+        resolve: {
+            extensions: ['.js'],
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@core': path.resolve(__dirname, 'src', 'core'),
+            }
+        },
+        plugins: [
+            new FaviconsWebpackPlugin('./favicon.png'),
+            new HtmlWebpackPlugin({
+                template: './index.html'
+            }),
+
+            new MiniCssExtractPlugin({
+                filename: filename('css'),
+            })
+        ],
+        devtool: isDev ? "source-map" : false,
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
                 }
-            }
-        ],
-    },
+            ],
+        },
+    };
 };
